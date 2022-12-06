3841cf47398af771837549f42c945f8d4f8a35a9
10Sadique <jafarsjahan@gmail.com>
2022-12-06 22:19:59 +0600

    add product to db

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // initializing server app and port
 const app = express();
@@ -10,6 +11,43 @@ const port = 5000;
 // middlewares
 app.use(cors());
 app.use(express.json());
+
+// mongoDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.onfc57d.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+    try {
+        const database = client.db('revinnsProducts');
+        const productCollection = database.collection('products');
+
+        /*---- Products ----*/
+        // Add new product
+        app.post('/products', async (req, res) => {
+            const product = req.body;
+
+            const query = { porductId: product.porductId };
+            const existingProduct = await productCollection.findOne(query);
+
+            // If product doesn't exists in db then add product
+            if (!product) {
+                const result = await productCollection.insertOne(product);
+
+                return res.send(result);
+            }
+            res.send({
+                acknowledged: false,
+            });
+        });
+    } finally {
+    }
+}
+
+run().catch((error) => console.log(error));
 
 // initialize '/' route
 app.get('/', (req, res) => {
